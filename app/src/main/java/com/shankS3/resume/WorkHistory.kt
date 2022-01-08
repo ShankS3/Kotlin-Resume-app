fca012087505fbd17a5c3bc5d1fe7c8e63485b31
ShankS3 <shashank.surwase@gmail.com>
2022-01-09 00:16:50 +0530

    Added activity work history

--- app/src/main/java/com/shankS3/resume/WorkHistory.kt
+++ app/src/main/java/com/shankS3/resume/WorkHistory.kt
@@ -1,11 +1,11 @@
-package com.ShankS3.resume
+package com.shankS3.resume
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class WorkHistory : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_work_history)
     }
 }
